0e72676b37376d0eda4a6e13b1b617cf952a7d6d
Domen Orosel <orosel.domen@gmail.com>
2014-05-14 13:35:27 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-	createDiv();
+	createGrid();
+
+	$("#top > #reset").on("click", function() {
+		$("#wrapper").find(".highlight").removeClass("highlight");
+		$("#wrapper").find(".cell").removeClass("cell");
+
+		createGrid();
+	})
+
+
+
+});
+
+function createGrid() {
+	var gridSize = prompt("Choose your grid size:");
+
+	for (var i = 0; i < gridSize*gridSize; i++) {
+		$("<div></div>").attr("class", "cell").appendTo("#wrapper");
+	};
 
 	$("#wrapper > .cell").on("mouseenter", function(){
 		$(this).removeClass("cell").addClass("highlight");
 	})
-
-});
-
-function createDiv() {
-	for (var i = 0; i < 16; i++) {
-		$("<div></div>").attr("class", "cell").appendTo("#wrapper");
-	};
-
 }
+
